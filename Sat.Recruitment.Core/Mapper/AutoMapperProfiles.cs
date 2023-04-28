513a7ae23edfc47006be9f3dc1210ced632cef8d
David Ramos <davidramos015@gmail.com>
2023-04-27 19:36:41 -0600

    Set up AutoMapper on Core Project

---
+++ Sat.Recruitment.Core/Mapper/AutoMapperProfiles.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Sat.Recruitment.Api.Core.DTOs;
+using Sat.Recruitment.Core.Abstract; 
+
+namespace Sat.Recruitment.Core.Mapper
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, RegisterUserDto>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            CreateMap<User, UserDto>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+
+            CreateMap<RegisterUserDto, User>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            CreateMap<RegisterUserDto, UserDto>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+      
+            CreateMap<UserDto, User>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            CreateMap<UserDto, RegisterUserDto>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+        }
+    }
+}
